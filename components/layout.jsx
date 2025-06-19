162ec6c2993504dab204fa73bec37e2badda8134
mlyshol <32914556+mlyshol@users.noreply.github.com>
2025-06-19 14:12:13 -0700

    Updates to Sermons Mobile

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,6 +1,7 @@
 // components/Layout.jsx
 import React from "react";
 import Head from "next/head";
+import Script from "next/script";
 import Navbar from "./navbar";
 
 export default function Layout({ children, pageTitle, pageDescription }) {
@@ -18,9 +19,11 @@ export default function Layout({ children, pageTitle, pageDescription }) {
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/logo.png" />
+      </Head>
+      <Script>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6903604640424521"
      crossorigin="anonymous"></script>
-      </Head>
+      </Script>
 
       <Navbar />
 
